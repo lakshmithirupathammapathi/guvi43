5fe79425c4bb426adb859136e0aa8c1d4ff1f933
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-08-28 10:07:49 +0530

    Create guvi43.java

---
+++ guvi43.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter first string");
+		String firstStr=sc.next();
+		System.out.println("enter second string");
+		String secStr=sc.next();
+		System.out.println("results after concatination ");
+		System.out.println(firstStr+" "+secStr);
+	
+}
+}
